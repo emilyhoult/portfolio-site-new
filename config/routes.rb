50848962fcbdf1c5d9bf1f7306785a77c6e36a9e
Emily Hoult <emily@emilyhoult.com>
2021-07-26 19:27:00 +0200

    add all images to photos page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get '/', to: 'homepage#home', as: 'root'
-  get 'photos/photos', to: 'photos#photos', as: 'photos'
+  get 'photos/', to: 'photos#photos', as: 'photos'
 end
