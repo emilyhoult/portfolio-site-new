ef355186e7173d92c52c9cf53c44588912a3f7d5
Emily Hoult <emily@emilyhoult.com>
2021-09-01 14:13:24 +0200

    add function for scroll top btn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -44,6 +44,23 @@ document.addEventListener('DOMContentLoaded', function() {
   })
 });
 
+$(function() {
+  var topBtn = $('.top-btn');
+  topBtn.hide();
+  $(window).scroll(function(){
+     if ($(this).scrollTop() > 100) {
+       topBtn.fadeIn(1000);
+     } else {
+       topBtn.fadeOut();
+     }
+  });
+  topBtn.click(function () {
+    $('body, html').animate({
+      scrollTop:0
+    }, 1000);
+    event.preventDefault();
+  });
+});
 
 
 
